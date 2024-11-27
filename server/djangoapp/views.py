17f32b0d89f5a5561e2f0394fb193373fb6a074c
Yousef <yousif0180@gmail.com>
2024-11-27 15:54:40 -0500

    adding user interface for dealing with the backend proxy and more ..

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -89,7 +89,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        print(car_model)
         cars.append({"CarModel": car_model.name, "CarMake":car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
@@ -105,19 +104,19 @@ def get_dealerships(request, state="All"):
 
 def get_dealer_details(request, id):
     if id:
-        endpoint = "/fetchDealers/"+id
-        dealerships = get_request(endpoint)
-        return JsonResponse({"status":200,"dealers":dealerships})
+        endpoint = "/fetchDealer/"+str(id)
+        print(endpoint)
+        dealership = get_request(endpoint)
+        return JsonResponse({"status":200,"dealer":dealership})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 def get_dealer_reviews(request, dealer_id):
     if dealer_id:
-        endpoint = "/fetchReviews/dealer/"+dealer_id
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review in reviews:
             response = analyze_review_sentiments(review["review"])
-            print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
